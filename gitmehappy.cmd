4165bbb88efb3f0415e3d072e7d0f9b94efcd449
Adrian Sims <adriansims62781@gmail.com>
2018-04-16 22:07:12 +0100

    Add project files.

---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add CrsDistance/Program.cs
+git add CrsDistance/App.config
+git add CrsDistance/Properties/AssemblyInfo.cs
+git add CrsDistance/CrsDistance.csproj
+git add CrsDistance.sln
+git add .gitignore
